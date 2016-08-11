c3c0606725653038ba01f836a71927181bff4718
masozow <marvingjo@hotmail.com>
2016-08-10 22:26:17 -0600

    Cambiando la URL de media_root para ya no usar el debug=true

--- sistemakadosh/urls.py
+++ sistemakadosh/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('kadoshapp.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
