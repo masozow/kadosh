a6b8acb5cc8f82bc73af102a6c9593072e6ccea4
masozow <marvingjo@hotmail.com>
2016-08-30 22:57:11 -0600

    Arreglando problemas de conexión en el servidor

--- sistemakadosh/settings.py
+++ sistemakadosh/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enriquesaju$kadosh2',
-        'USER': 'root',
+        'USER': 'enriquesaju',
         'PASSWORD': 'hola1234',
         'HOST': 'localhost',
         'PORT': '',
